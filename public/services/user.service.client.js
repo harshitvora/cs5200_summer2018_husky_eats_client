c8199f7f46c9f421cc7f430bb33eb55b322672c8
harshitvora <harshit.hvora@gmail.com>
2018-08-19 22:42:37 -0400

    Cors error fix header

--- public/services/user.service.client.js
+++ public/services/user.service.client.js
@@ -80,7 +80,7 @@
         }
 
         function deleteUser(userId) {
-            return $http.delete({url:SERVER_URL + "/api/user/"+userId, headers:{'Content-Type':'application/json'}})
+            return $http.delete(SERVER_URL + "/api/user/"+userId, {headers:{'Content-Type':'application/json'}})
                 .then(function (response) {
                     return response.data;
                 });
